1e4f98da99f95b01fbdb0a5c3f16a96a97dcddaa
CozmoCostanza <12537305+tplesetz@users.noreply.github.com>
2024-07-08 15:46:21 -0500

    adding widget extension

---
+++ ThingWidget/ThingWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ThingWidgetBundle.swift
+//  ThingWidget
+//
+//  Created by Tyler Plesetz on 7/8/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ThingWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ThingWidget()
+        ThingWidgetControl()
+    }
+}
