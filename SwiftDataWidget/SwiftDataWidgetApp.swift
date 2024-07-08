294bcf812ad490bea1e616ef39000c2acc48d4a8
CozmoCostanza <12537305+tplesetz@users.noreply.github.com>
2024-07-08 11:47:11 -0500

    Initial Commit

---
+++ SwiftDataWidget/SwiftDataWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataWidgetApp.swift
+//  SwiftDataWidget
+//
+//  Created by Tyler Plesetz on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
